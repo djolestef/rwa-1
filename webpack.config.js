ba07705121b68355efcd6509ea62c8e6a61cc1e5
Djordje Stefanovic <stefanovicdjordje25@gmail.com>
2020-05-01 20:35:32 +0200

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.js'],
+
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './',
+  },
+  module: {
+    rules: [
+      {
+        //kako ce se obraditi fajlovi
+        test: /\.js$/, //regularni izraz $-ako se string zavrsava sa tom reci
+        exclude: /(node_modules|bower_components)/, //da ne trazi node_modules jer ima mng fajlova
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
